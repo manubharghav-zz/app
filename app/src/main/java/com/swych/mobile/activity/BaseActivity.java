0990b523bcdbe4b0e37b4c66f05fa9e74dee6ef7
Manu Reddy <manubharghav@gmail.com>
2015-07-13 02:00:00 -0700

    fixed the issue of improper animation on the navigation drawer

--- app/src/main/java/com/swych/mobile/activity/BaseActivity.java
+++ app/src/main/java/com/swych/mobile/activity/BaseActivity.java
@@ -2,10 +2,13 @@ package com.swych.mobile.activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -18,14 +21,14 @@ import android.widget.Toast;
 
 import com.swych.mobile.R;
 
+
 /**
- * @author dipenp
  *
  * This activity will add Navigation Drawer for our application and all the code related to navigation drawer.
  * We are going to extend all our other activites from this BaseActivity so that every activity will have Navigation Drawer in it.
  * This activity layout contain one frame layout in which we will add our child activity layout.  
  */
-public class BaseActivity extends ActionBarActivity {
+public class BaseActivity extends AppCompatActivity {
 
     /**
      *  Frame layout: Which is going to be used as parent layout for child activity layout.
@@ -43,8 +46,7 @@ public class BaseActivity extends ActionBarActivity {
     /**
      * List item array for navigation drawer items.
      * */
-    protected String[] listArray = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
-
+    protected String[] navigationalPlaces;
     /**
      * Static variable for selected item position. Which can be used in child activity to know which item is selected from the list.
      * */
@@ -75,12 +77,10 @@ public class BaseActivity extends ActionBarActivity {
         frameLayout = (FrameLayout)findViewById(R.id.content_frame);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
-
+        navigationalPlaces = getResources().getStringArray(R.array.navigation_places);
         // set a custom shadow that overlays the main content when the drawer opens
-        //mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
-
         // set up the drawer's list view with items and click listener
-        mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, listArray));
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, navigationalPlaces));
         mDrawerList.setOnItemClickListener(new OnItemClickListener() {
 
             @Override
@@ -91,58 +91,53 @@ public class BaseActivity extends ActionBarActivity {
             }
         });
 
-        // enable ActionBar app icon to behave as action to toggle nav drawer
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
+
+
+
 
         // ActionBarDrawerToggle ties together the the proper interactions between the sliding drawer and the action bar app icon
         actionBarDrawerToggle = new ActionBarDrawerToggle(
                 this,						/* host Activity */
                 mDrawerLayout, 				/* DrawerLayout object */
-                R.drawable.ic_launcher,     /* nav drawer image to replace 'Up' caret */
                 R.string.navigation_drawer_open,       /* "open drawer" description for accessibility */
                 R.string.navigation_drawer_close)      /* "close drawer" description for accessibility */
         {
             @Override
             public void onDrawerClosed(View drawerView) {
-                getSupportActionBar().setTitle(listArray[position]);
-                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
                 super.onDrawerClosed(drawerView);
+                getSupportActionBar().setTitle(navigationalPlaces[position]);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+
             }
 
             @Override
             public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
                 getSupportActionBar().setTitle(getString(R.string.app_name));
                 invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-                super.onDrawerOpened(drawerView);
             }
 
-            @Override
-            public void onDrawerSlide(View drawerView, float slideOffset) {
-                super.onDrawerSlide(drawerView, slideOffset);
-            }
-
-            @Override
-            public void onDrawerStateChanged(int newState) {
-                super.onDrawerStateChanged(newState);
-            }
         };
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
         mDrawerLayout.setDrawerListener(actionBarDrawerToggle);
 
+        // enable ActionBar app icon to behave as action to toggle nav drawer
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
 
         /**
          * As we are calling BaseActivity from manifest file and this base activity is intended just to add navigation drawer in our app.
          * We have to open some activity with layout on launch. So we are checking if this BaseActivity is called first time then we are opening our first activity.
          * */
-        if(isLaunch){
-            /**
-             *Setting this flag false so that next time it will not open our first activity.
-             *We have to use this flag because we are using this BaseActivity as parent activity to our other activity.
-             *In this case this base activity will always be call when any child activity will launch.
-             */
-            isLaunch = false;
-            openActivity(0);
-        }
+//        if(isLaunch){
+//            /**
+//             *Setting this flag false so that next time it will not open our first activity.
+//             *We have to use this flag because we are using this BaseActivity as parent activity to our other activity.
+//             *In this case this base activity will always be call when any child activity will launch.
+//             */
+//            isLaunch = false;
+//            openActivity(0);
+//        }
     }
 
     /**
@@ -158,25 +153,31 @@ public class BaseActivity extends ActionBarActivity {
          * it will reset this value because of initialization in onCreate method.
          * So that we are setting this in child activity.
          */
+        //TODO check this.. Important. Mostly its not needed but still check once more before deleting it.
 //		mDrawerList.setItemChecked(position, true);
 //		setTitle(listArray[position]);
-        mDrawerLayout.closeDrawer(mDrawerList);
+//        mDrawerLayout.closeDrawer(mDrawerList);
         BaseActivity.position = position; //Setting currently selected position in this field so that it will be available in our child activities.
 
         switch (position) {
             case 0:
-                Toast.makeText(getApplicationContext(),"Selected Item Position::"+position, Toast.LENGTH_SHORT).show();
+                //BookStore
+
                 break;
             case 1:
+                //Library
                 Toast.makeText(getApplicationContext(),"Selected Item Position::"+position, Toast.LENGTH_SHORT).show();
                 break;
             case 2:
+                //Favourites
                 Toast.makeText(getApplicationContext(),"Selected Item Position::"+position, Toast.LENGTH_SHORT).show();
                 break;
             case 3:
+                //Settings
                 Toast.makeText(getApplicationContext(),"Selected Item Position::"+position, Toast.LENGTH_SHORT).show();
                 break;
             case 4:
+                //About
                 Toast.makeText(getApplicationContext(),"Selected Item Position::"+position, Toast.LENGTH_SHORT).show();
                 break;
 
@@ -188,10 +189,23 @@ public class BaseActivity extends ActionBarActivity {
     }
 
     @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
         getMenuInflater().inflate(R.menu.menu_base, menu);
-        return super.onCreateOptionsMenu(menu);
+        return true;
     }
 
     @Override
